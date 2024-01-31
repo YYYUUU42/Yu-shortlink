2226c031354c283ec8fe21b513ff35137693639a
yu <2758345412@qq.com>
2024-01-31 16:44:10 +0800

    Initial commit

---
+++ admin/src/main/java/com/yu/admin/dao/mapper/GroupMapper.java
@@ -0,0 +1,13 @@
+
+
+package com.yu.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yu.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+
+ */
+public interface GroupMapper extends BaseMapper<GroupDO> {
+}
---
+++ admin/src/main/java/com/yu/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+
+
+package com.yu.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yu.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}
